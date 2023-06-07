e088b7940a2f4fbaa1042a57326ed8a24cc38e4d
GIT13Olena <girnyak.1olena@gmail.com>
2023-06-07 12:09:45 +0300

    07/06/12:09

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,20 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    if (contacts) {
+      this.setState({ contacts: JSON.parse(contacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
   addContact = ({ name, number }) => {
     const { contacts } = this.state;
 
@@ -37,20 +51,6 @@ class App extends Component {
       JSON.stringify([...this.state.contacts, contact])
     );
   };
-
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    if (contacts) {
-      this.setState({ contacts: JSON.parse(contacts) });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-    if (prevState.contacts !== contacts) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }
 
   changeFilter = event => {
     this.setState({ filter: event.currentTarget.value });
